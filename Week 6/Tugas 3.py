97f8cf86bef9b2762f3e8321872c910495219df7
fatwaar <fatwaauliarachma@mail.ugm.ac.id>
2024-09-20 08:34:31 +0700

    Update Tugas 3.py

--- Week 6/Tugas 3.py
+++ Week 6/Tugas 3.py
@@ -2,7 +2,7 @@ A={100,7,8}
 B={200,4,5}
 C={300,2,3}
 D={100,200,300}
-print(A&D)
-print(B&D)
-print(C&D)
-print(A&B&C&D)
+print("A∩D =",A&D)
+print("B∩D =",B&D)
+print("C∩D =",C&D)
+print("A∩B∩C∩D =",A&B&C&D)
