a309208a87ccf0d2c2cc09ce73355869b84dc870
fatwaar <fatwaauliarachma@mail.ugm.ac.id>
2024-09-20 08:55:27 +0700

    Update Tugas 5.py

--- Week 6/Tugas 5.py
+++ Week 6/Tugas 5.py
@@ -1,7 +1,7 @@
 A={'a', 'b', 'c', 'd'}
 B={'c', 'd', 'e' }
 C={}
-print(A.symmetric_difference(B))
-print(B.symmetric_difference(A))
-print(A.symmetric_difference(C))
-print(B.symmetric_difference(C))
+print("A symmetric difference B =",A.symmetric_difference(B))
+print("B symmetric difference A =",B.symmetric_difference(A))
+print("A symmetric difference C =",A.symmetric_difference(C))
+print("B symmetric difference C =",B.symmetric_difference(C))
